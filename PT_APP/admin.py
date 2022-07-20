bead547578746f67e52bc3692a7877c442b13f4a
MichaelLetson <mletsongamble@gmail.com>
2022-07-20 22:14:34 +0000

    create base.html using initial provided templat

--- PT_APP/admin.py
+++ PT_APP/admin.py
@@ -6,5 +6,8 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
 
+    list_display = ('name', 'slug', 'status', 'created_on')
+    search_fields = ['name', 'content']
+    list_filter = ('created_on',)
     prepopulated_fields = {'slug': ('name',)}
     summernote_fields = ('content',)
