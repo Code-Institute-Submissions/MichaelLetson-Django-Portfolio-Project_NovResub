7048b41fa7dceb8d78e9e525df844d198f1be143
MichaelLetson <mletsongamble@gmail.com>
2022-07-28 13:03:23 +0000

    flesh out html for post_detail.html

--- PT_APP/views.py
+++ PT_APP/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Post
 
 
@@ -8,3 +8,17 @@ class PostList(generic.ListView):
     queryset = Post.objects.order_by('-created_on')
     template_name = 'index.html'
     paginate_by = 5
+
+
+class PostDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        post = get_object_or_404(slug=slug)
+        
+        return render(
+            request,
+            "post_detail.html",
+            {
+                "post": post,
+            },
+        )
