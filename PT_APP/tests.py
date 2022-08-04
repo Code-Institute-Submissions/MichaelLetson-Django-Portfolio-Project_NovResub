bb909ca77624f4ce47d5667374721640045743d6
MichaelLetson <mletsongamble@gmail.com>
2022-08-04 20:17:17 +0000

    corrected styling on card padding, and card text size for non authenticated users

--- PT_APP/tests.py
+++ PT_APP/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from .models import Post
 
 # Create your tests here.
+
+class TestAppModels(TestCase):
+
+    def test_model_str(self):
+        name = Post.objects.create(name="Django Testing")
+        self.assertEqual(str(name), "Django Testing")
+        
