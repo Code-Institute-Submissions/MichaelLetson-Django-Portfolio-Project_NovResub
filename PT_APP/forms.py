45b6b1afdbc6c98da02b85240a0388b85864dc06
MichaelLetson <mletsongamble@gmail.com>
2022-08-07 01:58:05 +0000

    corrected line length issues in all files

--- PT_APP/forms.py
+++ PT_APP/forms.py
@@ -14,9 +14,9 @@ class NewPostForm(forms.ModelForm):
         }
 
         widgets = {
-            'name': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Enter your name'}),
-            'location':forms.TextInput(attrs={'class':'form-control', 'placeholder':'Location'}),
-            'price':forms.NumberInput(attrs={'class':'form-control', 'placeholder':'Session price per hour'}),
-            'content':forms.Textarea(attrs={'class':'form-control', 'placeholder':'Tell us about you...'}),
+            'name': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Enter your name'}), # noqa
+            'location':forms.TextInput(attrs={'class':'form-control', 'placeholder':'Location'}), # noqa
+            'price':forms.NumberInput(attrs={'class':'form-control', 'placeholder':'Session price per hour'}), # noqa
+            'content':forms.Textarea(attrs={'class':'form-control', 'placeholder':'Tell us about you...'}), # noqa
         }
 
